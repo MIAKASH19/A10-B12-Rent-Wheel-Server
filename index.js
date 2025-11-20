a6ff0c426363ece986f118f6f8610556ce9e0761
Mi Akash <mszharul1990@gmail.com>
2025-11-21 02:07:29 +0600

    final commit

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
     console.log("Connected to MongoDB");
 
     const database = client.db("rent-wheels-DB");
